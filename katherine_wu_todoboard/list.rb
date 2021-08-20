4716e79e1e4e3af47edc6c75063960533243f934
kw-8 <kwu11235813@gmail.com>
2021-08-20 12:53:45 -0700

    list done

--- katherine_wu_todoboard/list.rb
+++ katherine_wu_todoboard/list.rb
@@ -1,3 +1,4 @@
+require "byebug"
 require_relative "item"
 
 class List
@@ -9,7 +10,7 @@ class List
     end
 
     def add_item(title, deadline, *description)
-        description ||= ""
+        description = ""
         if Item.valid_date?(deadline)
             @items << Item.new(title, deadline, description)
             true
@@ -41,6 +42,66 @@ class List
         @items.first
     end
 
-    # def print
+    def print
+        puts "="*35 + "\n" + @label.center(35) + "\n" + "-"*35
+        @items.each_with_index{|item, index| puts index.to_s.ljust(5) + item.title.ljust(20) + item.deadline.ljust(10)}
+        puts "="*35
+    end
 
+    def print_full_item(index)
+        item = @items[index]
+        puts "-"*35 + "\n" + item.title.ljust(25) + item.deadline
+        puts item.description
+        puts "-"*35
+    end
+
+    def print_priority
+        print_full_item(0)
+    end
+
+    def up(index, *amount)
+        amount = amount.first || 1
+        if valid_index?(index)
+            while amount > 0
+                swap(index, index-1)
+                index -= 1 if index > 0
+                amount -= 1
+            end
+            true
+        else false
+        end
+    end
+
+    def down(index, *amount)
+        amount = amount.first || 1
+        # debugger
+        if valid_index?(index)
+            while amount > 0
+                swap(index, index+1)
+                index += 1 if index <= @items.length
+                amount -= 1
+            end
+            true
+        else false
+        end
+    end
+
+    def sort_by_date
+        @items.sort_by!{|item| item.deadline}
+    end
 end
+
+# p liszt = List.new("liszt's daily diet")
+# p liszt.add_item("cabbage", "1835-01-01")
+# p liszt.add_item("wurst", "1835-01-02", "a sausage bought down the street")
+# p liszt.add_item("composition paper", "1835-01-04")
+# p liszt.add_item("coffee", "1835-01-03")
+# p liszt.size
+# p liszt.print
+# p liszt.swap(2,3)
+# p liszt.print
+# p liszt.print_full_item(1)
+# p liszt.down(0)
+# p liszt.print
+# p liszt.sort_by_date
+# p liszt.print
